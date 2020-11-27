0a3a2337e94f61cbadf2f39fc435982ddd56cc08
mohamedmorsi95 <mohamedmorsi9595@gmail.com>
2020-11-27 06:47:10 +0200

    Add files via upload

---
+++ Config.php
@@ -0,0 +1,10 @@
+<?php 
+	define('DB_USERNAME','root');
+	define('DB_PASSWORD','');
+	define('DB_NAME','push_notification');
+	define('DB_HOST','localhost');
+
+	//defined a new constant for firebase api key
+	define('FIREBASE_API_KEY', 'server key');
+
+	
